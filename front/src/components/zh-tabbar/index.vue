19b2d4f49c9fbcbd3f686f7e1198735419a90f8f
张中华 <956159241@qq.com>
2023-05-25 16:28:09 +0800

    优化首页

--- front/src/components/zh-tabbar/index.vue
+++ front/src/components/zh-tabbar/index.vue
@@ -1,22 +1,48 @@
 <template>
     <view class="content">
-        <view v-for="(item, index) in list">
+        <!-- <view class="item" v-for="(item, index) in list">
             <view>{{ item }}</view>
-        </view>
-        <van-button type="primary">测试按钮</van-button>
-
+        </view> -->
     </view>
 </template>
 
 <script lang="ts" setup>
-    import { ref } from 'vue';
+    // import { ref, PropType } from 'vue';
+    // const list = ['1', '2', '3'];
+    // interface ITab {
+    //     active: number
+    //     fixed: boolean
+    // }
 
-    const list = ['1', '2', '3'];
+    // const props = defineProps({
+    //     items: {
+    //         type: String
+    //     },
+//   formSettings: {
+//     type: Object as any, //PropType<FormSettingsModel>
+//     required: true, // 必传
+//   },
+
+//   tableSettingPanel: {
+//     type: Object as PropType<TableSettingPanelModel> | undefined,
+//   },
+
+//   tableSettings: {
+//     type: Object as PropType<TableSettingsModel>,
+//     required: true, // 必传
+//   },
+
+//   pageSettings: {
+//     type: Object as PropType<pageSettingsModel>,
+//     required: true, // 必传
+//   },
+// });
 </script>
 
 <style lang="scss" scoped>
  .content {
     height: 30px;
     width: 100%;
+    display: flex;
  }
 </style>
--- front/src/pages/index/index.vue
+++ front/src/pages/index/index.vue
@@ -1,30 +1,38 @@
 <template>
   <view class="content">
-    <swiper class="swiper" circular :indicator-dots="true" :autoplay="true" :interval="5000"
-				:duration="1000">
-				<swiper-item v-for="(item, index) in imgList" :key="index">
-					<image :src="item" class="image"></image>
-				</swiper-item>
-			</swiper>
-      <zhtabbar></zhtabbar>
+    <swiper class="swiper" circular :indicator-dots="true" :autoplay="true" :interval="5000" :duration="1000">
+      <swiper-item v-for="(item, index) in imgList" :key="index">
+        <image :src="item" class="image"></image>
+      </swiper-item>
+    </swiper>
+    <view class="tab-bar" style="width: 100%;">
+      <van-notice-bar style="width: 100%;" text="足协杯战线连续第2年上演广州德比战，上赛季半决赛上恒大以两回合5-3的总比分淘汰富力。" />
+    </view>
+
+    <van-tabbar>
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </view>
 </template>
 
 <script setup>
-	import { ref, onMounted } from 'vue'
-  import { onLoad, onShow } from "@dcloudio/uni-app";
-  import zhtabbar from '../../components/zh-tabbar/index.vue';
+import { ref, onMounted } from 'vue'
+import { onLoad, onShow } from "@dcloudio/uni-app";
+const active = ref(0);
 
-  const imgList = [
-    'http://qiniu-tujieqianduan.iotzzh.com/cloud-g5b019eb1b_1920.jpg',
-    'http://qiniu-tujieqianduan.iotzzh.com/lake-gc521668c6_1920.jpg',
-    'http://qiniu-tujieqianduan.iotzzh.com/nature-g978431efb_1920.jpg',
-  ];
+const imgList = [
+  'http://qiniu-tujieqianduan.iotzzh.com/cloud-g5b019eb1b_1920.jpg',
+  'http://qiniu-tujieqianduan.iotzzh.com/lake-gc521668c6_1920.jpg',
+  'http://qiniu-tujieqianduan.iotzzh.com/nature-g978431efb_1920.jpg',
+];
 
-  onShow(() => {
-    wx.hideHomeButton();
-  });
-	
+onShow(() => {
+  wx.hideHomeButton();
+});
+
 </script>
 
 <style scoped lang="scss">
@@ -33,9 +41,11 @@
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
   .swiper {
     width: 100%;
     height: 300rpx;
+
     .image {
       width: 100%;
       height: 100%;
