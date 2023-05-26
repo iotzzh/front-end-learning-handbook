bf407870eb0c7177c94416d4855965435c07092f
zhangzhonghua <956159241@qq.com>
2023-05-26 16:21:41 +0800

    添加首页的一些信息

---
+++ front/src/components/zh-van-tabbar/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <van-tabbar :active="active" @change="onChange">
+        <van-tabbar-item icon="gem-o">推荐</van-tabbar-item>
+        <van-tabbar-item icon="newspaper-o">测试</van-tabbar-item>
+        <van-tabbar-item icon="wap-home-o">首页</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">学习</van-tabbar-item>
+        <van-tabbar-item icon="user-circle-o">我的</van-tabbar-item>
+    </van-tabbar>
+</template>
+
+<script lang="ts" setup>
+import { computed, toRefs } from 'vue';
+
+const props = defineProps({
+    active: {
+        // type: Object as PropType<any>,
+        type: Number,
+        required: true,
+    },
+});
+
+const { active } = toRefs(props);
+const emit = defineEmits(['change:active']);
+
+
+const onChange = (event:any) => {
+    emit('change:active', event.detail);
+}
+
+</script>
--- front/src/pages/index/index.vue
+++ front/src/pages/index/index.vue
@@ -9,19 +9,38 @@
       <van-notice-bar style="width: 100%;" text="一年之计在于春，一日之计在于晨。早起的鸟有虫吃，是时候起床了~" />
     </view>
 
-    <van-tabbar>
-      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="search">标签</van-tabbar-item>
-      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
-    </van-tabbar>
+    <view style="width: 100%; display: flex; height: 300rpx;">
+      <view style="flex: 1; padding: 10px 10px 0 0; background-color: blue;" >打卡区域
+        <view>本年打卡：xxx</view></view>
+      <view style="flex: 1; padding: 10px 10px 0 0; background-color: lightblue; display: flex; flex-direction: column;" >
+        <view>全国排名（问号）: 打卡xx次，题目 xx次</view>
+        <view>本市排名（问号）：打卡xx次，题目 xx次</view>
+        <view>考试题数：xxx</view>
+      </view>
+    </view>
+
+    <view style="width: 100%; display: flex; height: 700rpx;">
+      <view style="flex: 1; padding: 10px 10px 0 0; background-color: pink;" >全国列表， 各20条</view>
+      <view style="flex: 1; padding: 10px 10px 0 0; background-color: lightgray; display: flex; flex-direction: column;" >
+        全市列表
+        各20条
+      </view>
+    </view>
+
+    <ZhVanTabbar v-model:active="active" @change:active="(value:number) => active = value"></ZhVanTabbar>
   </view>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { onLoad, onShow } from "@dcloudio/uni-app";
-const active = ref(0);
+import ZhVanTabbar from '../../components/zh-van-tabbar/index.vue';
+const active = ref(2);
+
+const onChange = (event:any) => {
+  console.log(event);
+}
+
 
 const imgList = [
   'http://qiniu-tujieqianduan.iotzzh.com/cloud-g5b019eb1b_1920.jpg',
